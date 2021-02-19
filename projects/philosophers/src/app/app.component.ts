0d69b576a42f59c3514937467f6ebfd23dbdad1f
plastikaweb <info@plastikaweb.com>
2021-02-19 19:19:01 +0100

    chore(add project philosophers and remove main project):

--- projects/philosophers/src/app/app.component.ts
+++ projects/philosophers/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'philo-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-solid';
+  title = 'philosophers';
 }
